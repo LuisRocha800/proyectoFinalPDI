8714dbd7631f113de61f3cb3281f39299df5afe9
stranger <stranger@192.168.1.71>
2023-06-19 21:59:20 -0500

    funcion abrir imagen ya reconoce el tipo, y adapte app

--- AppDesigner/abrirImagen.m
+++ AppDesigner/abrirImagen.m
@@ -1,23 +1,45 @@
 %%
 % FUNCIÓN: "abrirImagen".
-% Abre, carga y muestra una imagen en el UIAxes indicado como parámetro
+% Abre, carga y muestra una imagen en la ventana (en el componente UIAxes 
+% indicado como parámetro).
 % 
 % Input:
 %       UIAxes: Componente de tipo 'app.UIAxes' donde se mostrará la imagen
 % Output:
-%       imagen: Matriz de la imagen abierta.
-%       mapa: mapa de color
-%       nombre: file name de la imagen abierta
-%       path: ruta de la imagen
-%       indx: extensión de la imagen
-function [imagen, mapa, nombre, path, indx] = abrirImagen(UIAxes)
+%       img:   Matriz de la imagen
+%       map:   mapa de color si es indexada o cero en caso contrario
+%       file:  nombre del archivo
+%       path:  ruta de la imagen
+%       type:  tipo de imagen (rgb, indexada, escala de grises o binaria)
 
-% Abrir ventana para seleccionar archivo con extensión permitida
-[nombre, path, indx] = uigetfile('*.jpg;*.tif;*.bmp;*.ppm;*.mdl;*.png','Abrir');
-if nombre==0
+function [img, map, path, type] = abrirImagen(UIAxes)
+
+% Abrir ventana para seleccionar imagen con extensión permitida
+[file, path] = uigetfile('*.jpg;*.tif;*.bmp;*.ppm;*.mdl;*.png','Abrir imagen');
+if file == 0
     return
 end
 
-% Colocar la imagen en el UIAxes correspondiente
-[imagen, mapa] = imread(fullfile(path, nombre));
-imshow(imagen, 'Parent', UIAxes);
+% Colocar la imagen en el UIAxes correspondiente, dependiendo su tipo
+if imfinfo(fullfile(path, file)).ColorType == "truecolor"
+    type = "truecolor";
+    map  = 0;
+    img  = imread(fullfile(path, file));
+    imshow(img, 'Parent', UIAxes);
+elseif imfinfo(fullfile(path, file)).ColorType == "indexed"
+    type = "indexed";
+    [img, map] = imread(fullfile(path, file));
+    imshow(img, map, 'Parent', UIAxes);
+elseif imfinfo(fullfile(path, file)).ColorType == "grayscale" 
+    if  imfinfo(fullfile(path, file)).BitDepth == 1 
+        type = "grayscale";
+        map  = 0;
+        img  = imread(fullfile(path, file));
+        imshow(img, 'Parent', UIAxes);
+    else
+        type = "grises";
+        map = 0;
+        img = imread(fullfile(path, file));
+        imshow(img, 'Parent', UIAxes);
+    end
+end
