d0467a7a73bd922e9ac53f3f2bd805ebadb54c22
stranger <stranger@192.168.1.71>
2023-06-19 23:48:58 -0500

    funcionalidad obtener info y cambio variables globales a privadas

--- AppDesigner/abrirImagen.m
+++ AppDesigner/abrirImagen.m
@@ -6,40 +6,42 @@
 % Input:
 %       UIAxes: Componente de tipo 'app.UIAxes' donde se mostrará la imagen
 % Output:
-%       img:   Matriz de la imagen
-%       map:   mapa de color si es indexada o cero en caso contrario
-%       file:  nombre del archivo
-%       path:  ruta de la imagen
-%       type:  tipo de imagen (rgb, indexada, escala de grises o binaria)
+%       img:  Matriz de la imagen
+%       map:  mapa de color si es indexada o cero en caso contrario
+%       path: ruta completa de la imagen
+%       type: tipo de imagen (rgb, indexada, escala de grises o binaria)
 
 function [img, map, path, type] = abrirImagen(UIAxes)
 
 % Abrir ventana para seleccionar imagen con extensión permitida
-[file, path] = uigetfile('*.jpg;*.tif;*.bmp;*.ppm;*.mdl;*.png','Abrir imagen');
+[file, folder] = uigetfile('*.jpg;*.tif;*.bmp;*.ppm;*.mdl;*.png;*.jpeg','Abrir imagen');
 if file == 0
     return
 end
 
+% Crear ruta completa de la imagen
+path = fullfile(folder, file);
+
 % Colocar la imagen en el UIAxes correspondiente, dependiendo su tipo
-if imfinfo(fullfile(path, file)).ColorType == "truecolor"
+if imfinfo(path).ColorType == "truecolor"
     type = "truecolor";
     map  = 0;
-    img  = imread(fullfile(path, file));
+    img  = imread(path);
     imshow(img, 'Parent', UIAxes);
-elseif imfinfo(fullfile(path, file)).ColorType == "indexed"
+elseif imfinfo(path).ColorType == "indexed"
     type = "indexed";
-    [img, map] = imread(fullfile(path, file));
+    [img, map] = imread(path);
     imshow(img, map, 'Parent', UIAxes);
-elseif imfinfo(fullfile(path, file)).ColorType == "grayscale" 
-    if  imfinfo(fullfile(path, file)).BitDepth == 1 
+elseif imfinfo(path).ColorType == "grayscale" 
+    if  imfinfo(path).BitDepth == 1 
         type = "grayscale";
         map  = 0;
-        img  = imread(fullfile(path, file));
+        img  = imread(path);
         imshow(img, 'Parent', UIAxes);
     else
         type = "grises";
         map = 0;
-        img = imread(fullfile(path, file));
+        img = imread(path);
         imshow(img, 'Parent', UIAxes);
     end
 end
